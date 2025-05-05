0da2b4931e3bf77fa85d9a3640280d43ed3b88d8
unknown <charliecha1990@gmail.com>
2025-05-05 15:58:05 -0400

    Revert "Fruit Table Goal 1"
    This reverts commit 1cd51fbc7579eb77e5d0cf929282042949379764.

--- src/app/fruit-challenge/components/fruit-table/fruit-table-view-model.ts
+++ src/app/fruit-challenge/components/fruit-table/fruit-table-view-model.ts
@@ -1,23 +1,13 @@
 import {Injectable} from '@angular/core';
 import {FruityViceService} from '../../services/fruity-vice-service';
 import {BehaviorSubject} from 'rxjs';
-import { Fruit } from '../../models/fruit';
+import {Fruit} from '../../models/fruit';
 
 @Injectable()
 export class FruitTableViewModel {
 
   fruitData$ = new BehaviorSubject<Fruit[]>(null);
   loadingFruit$ = new BehaviorSubject<boolean>(false);
-  columns = [
-    { name: 'name', label: 'Name', valueGetter: (fruit: Fruit) => fruit.name },
-    { name: 'id', label: 'ID', valueGetter: (fruit: Fruit) => fruit.id },
-    { name: 'genus', label: 'Genus', valueGetter: (fruit: Fruit) => fruit.genus },
-    { name: 'calories', label: 'Calories', valueGetter: (fruit: Fruit) => fruit.nutritions.calories },
-    { name: 'carbohydrates', label: 'Carbohydrates', valueGetter: (fruit: Fruit) => fruit.nutritions.carbohydrates },
-    { name: 'sugar', label: 'Sugar', valueGetter: (fruit: Fruit) => fruit.nutritions.sugar }
-  ]; // define the columns for the table
-
-  columnsToDisplay = this.columns.map(col => col.name); // map to get the column names
 
   constructor(private fruitService: FruityViceService) {
     this.loadingFruit$.next(true);
--- src/app/fruit-challenge/models/fruit.ts
+++ src/app/fruit-challenge/models/fruit.ts
@@ -8,3 +8,4 @@ export interface Fruit {
   order: string;
   nutritions: NutritionInfo;
 }
+
