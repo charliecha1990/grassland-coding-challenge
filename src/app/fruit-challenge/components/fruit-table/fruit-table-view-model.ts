091d9c0623bda12e8e88fd850c95fe766aa5fa4d
unknown <charliecha1990@gmail.com>
2025-05-05 10:34:56 -0400

    Fruit Table Goal 1

--- src/app/fruit-challenge/components/fruit-table/fruit-table-view-model.ts
+++ src/app/fruit-challenge/components/fruit-table/fruit-table-view-model.ts
@@ -8,6 +8,16 @@ export class FruitTableViewModel {
 
   fruitData$ = new BehaviorSubject<Fruit[]>(null);
   loadingFruit$ = new BehaviorSubject<boolean>(false);
+  columns = [
+    { name: 'name', label: 'Name', valueGetter: (fruit: Fruit) => fruit.name },
+    { name: 'id', label: 'ID', valueGetter: (fruit: Fruit) => fruit.id },
+    { name: 'genus', label: 'Genus', valueGetter: (fruit: Fruit) => fruit.genus },
+    { name: 'calories', label: 'Calories', valueGetter: (fruit: Fruit) => fruit.nutritions.calories },
+    { name: 'carbohydrates', label: 'Carbohydrates', valueGetter: (fruit: Fruit) => fruit.nutritions.carbohydrates },
+    { name: 'sugar', label: 'Sugar', valueGetter: (fruit: Fruit) => fruit.nutritions.sugar }
+  ]; // define the columns for the table
+
+  columnsToDisplay = this.columns.map(col => col.name); // map to get the column names
 
   constructor(private fruitService: FruityViceService) {
     this.loadingFruit$.next(true);
--- src/app/fruit-challenge/components/fruit-table/fruit-table.component.ts
+++ src/app/fruit-challenge/components/fruit-table/fruit-table.component.ts
@@ -8,11 +8,7 @@ import {FruitTableViewModel} from './fruit-table-view-model';
   providers: [FruitTableViewModel]
 })
 export class FruitTableComponent implements OnInit {
-  columnsToDisplay = ['id', 'name', 'genus', 'calories'];
-
-  constructor(public viewModel: FruitTableViewModel) {
-
-  }
+  constructor(public viewModel: FruitTableViewModel) {}
 
   ngOnInit(): void {
   }
